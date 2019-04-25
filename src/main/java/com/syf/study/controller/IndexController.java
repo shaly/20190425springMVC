d57c3d7f4e78c42f9a299e01c2ec8f8910a336f7
shaly <283199611@qq.com>
2019-04-25 23:59:53 +0800

    springMVC多种请求方式

--- src/main/java/com/syf/study/controller/IndexController.java
+++ src/main/java/com/syf/study/controller/IndexController.java
@@ -3,21 +3,37 @@ package com.syf.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.syf.study.entity.User;
 
 @Controller
 public class IndexController {
-	
-	
+
 	@RequestMapping("/index")
-	public String index(ModelMap model) {
-		System.out.println("访问首页***********");
+	public String index() {	
+		return "index";
+	}
+	
+	@RequestMapping("/index1")
+	public String index1(ModelMap model) {
+		System.out.println("访问首页1***********");
 		User u=new User();
 		u.setAccount("123456789");
 		u.setUsername("Jane");
 		model.addAttribute("u",u);		
 		return "index";
 	}
-	
+
+	@RequestMapping("/index2")
+	public ModelAndView index2() {
+		System.out.println("访问首页2***********");
+		ModelAndView m=new ModelAndView();
+		User u=new User();
+		u.setAccount("123456789");
+		u.setUsername("Jane");
+		m.addObject("u",u);	
+		m.setViewName("index");
+		return m;
+	}
 }
