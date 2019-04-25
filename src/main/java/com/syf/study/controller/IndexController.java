1093dfb07cd197ccb5e7104244ad87d1576bdc8e
shaly <283199611@qq.com>
2019-04-26 00:04:38 +0800

    springMVC多种请求方式forward

--- src/main/java/com/syf/study/controller/IndexController.java
+++ src/main/java/com/syf/study/controller/IndexController.java
@@ -19,7 +19,7 @@ public class IndexController {
 	public String index1(ModelMap model) {
 		System.out.println("访问首页1***********");
 		User u=new User();
-		u.setAccount("123456789");
+		u.setAccount("1");
 		u.setUsername("Jane");
 		model.addAttribute("u",u);		
 		return "index";
@@ -30,10 +30,22 @@ public class IndexController {
 		System.out.println("访问首页2***********");
 		ModelAndView m=new ModelAndView();
 		User u=new User();
-		u.setAccount("123456789");
+		u.setAccount("2");
 		u.setUsername("Jane");
 		m.addObject("u",u);	
 		m.setViewName("index");
 		return m;
 	}
+	
+	@RequestMapping("/index3")
+	public String index3(ModelMap model) {
+		System.out.println("访问首页3***********");
+		User u=new User();
+		u.setAccount("3");
+		u.setUsername("Jane");
+		model.addAttribute("u",u);		
+		return "forward:/WEB-INF/forward_page/index.jsp";
+	}
+	
+	
 }
