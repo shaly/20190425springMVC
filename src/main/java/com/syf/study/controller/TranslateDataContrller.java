3769b731f90df0ffdde5266d579ea15b5757a704
shaly <283199611@qq.com>
2019-05-06 21:40:46 +0800

    Controller请求参数：body传Date类型请求报错

---
+++ src/main/java/com/syf/study/controller/TranslateDataContrller.java
@@ -0,0 +1,31 @@
+package com.syf.study.controller;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.syf.study.entity.User;
+
+/**
+ * 基础数据类型转换
+ * @author Administrator
+ *
+ */
+@RestController
+public class TranslateDataContrller {
+	
+	//http://localhost:8080/springMVC/tDate?createTime=1990-01-01  //请求时页面会报错
+	@RequestMapping("/tDate")
+	public User tDate(User d) {
+		System.out.println(d);
+		return d;
+	}
+	
+
+
+}
